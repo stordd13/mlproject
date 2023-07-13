c8364eb26dd38742e0aa37e1bf200a64563c9473
stordd13 <stordeur.bru@gmail.com>
2023-07-13 14:20:57 +0200

    Flask app and prediction pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -11,6 +11,8 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_ingestion import DataIngestionConfig, DataIngestion
+from src.components.model_trainer import ModelTrainer
 
 from src.utils import save_object
 
@@ -27,7 +29,7 @@ class DataTransformation:
         This function is responsible for data transformation
         '''
         try:
-            numerical_features = ["writing score", "reading score"]
+            numerical_features = ["reading score", "writing score"]
             categorical_features = [
                 "gender",
                 "race/ethnicity",
@@ -44,11 +46,20 @@ class DataTransformation:
             )
             logging.info("Numerical columns standard scaling completed")
 
+
+            categories = [
+                            ["female", "male"],  # categories for "gender"
+                            ["group A", "group B", "group C", "group D", "group E"],  # categories for "race/ethnicity"
+                            ["some high school", "high school", "some college", 
+                             "associate's degree", "bachelor's degree", "master's degree"],  # categories for "parental level of education"
+                            ["free/reduced", "standard"],  # categories for "lunch"
+                            ["none", "completed"],  # categories for "test preparation course"
+]
             cat_pipeline = Pipeline(
                 steps=[
                     ("imputer", SimpleImputer(strategy='most_frequent')),
-                    ("one_hot_encoder", OneHotEncoder()),
-                    ("scaler", StandardScaler(with_mean=False))
+                    ("one_hot_encoder", OneHotEncoder(categories=categories, handle_unknown='error')
+                     )
                 ]
             )
 
@@ -71,15 +82,22 @@ class DataTransformation:
         try:
             train_df = pd.read_csv(train_path)
             test_df = pd.read_csv(test_path)
+            
             logging.info("'Read train and test data completed")
 
             logging.info("Obtaining preprocessing object")
 
             preprocessor_obj = self.get_data_transformer_object()
-
+    
             target_column_name = "math score"
-            numerical_columns = ["writing score", "reading score"]
-
+            numerical_columns = ["reading score", "writing score"]
+            categorical_columns = [
+                "gender",
+                "race/ethnicity",
+                "parental level of education",
+                "lunch",
+                "test preparation course"
+            ]
             input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
             target_feature_train_df = train_df[target_column_name]
 
@@ -89,6 +107,8 @@ class DataTransformation:
             logging.info(f"Applying preprocessing object on training and testing dataframes")
             input_feature_train_arr = preprocessor_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr = preprocessor_obj.transform(input_feature_test_df)
+
+            print(preprocessor_obj.transformers_)
 
             train_arr = np.c_[input_feature_train_arr, target_feature_train_df]
             test_arr = np.c_[input_feature_test_arr, target_feature_test_df]
@@ -108,4 +128,12 @@ class DataTransformation:
         except Exception as e:
             raise CustomException(e,sys) from e
         
-        i
+if __name__ == "__main__":
+    obj=DataIngestion()
+    train_data , test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
